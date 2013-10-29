bcb410a16718527de4fd2fca02492b67905e4ae3
Nick P Smith <npsmith1990@gmail.com>
2013-10-29 11:05:06 -0500

    Add binding.pry to Gemfile

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,6 +1,9 @@
 module CommentsHelper
+  # Displays next-page button if comments length is greater than the number of comments that can be contained on one page
+  #
+  # Returns nothing
   def next_page_button(&block)
     capture(&block) if @comments.length > @comment_limit
+    #binding.pry
   end
-
 end
